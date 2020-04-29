89721f6d25386298b556fa88da7bab3202c8dbac
JoaquinTR <joaquintricerri@gmail.com>
2020-04-29 10:49:49 -0300

    Adiciones, lógica, css, optimización

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+// --- INICIALIZACIÓN --- //
+
+document.getElementById("visibleCheck").checked = false;    //se me guardaba entre reloads
 var mellt = new Mellt();
 
 var userAgent = navigator.userAgent.toLowerCase();
@@ -5,6 +8,7 @@ var android = userAgent.indexOf("android") > -1;
 if(android){
     document.body.style.zoom = screen.logicalXDPI / screen.deviceXDPI;
 }
+console.log("User agent: "+ userAgent + " android: "+android);
 
 /*de ultima intentar usar .androidText:
 var htmlEl = document.getElementsByClassName('my-nice-class'); 
@@ -13,16 +17,39 @@ htmlEl.classList.add("my-nice-class--dimensions_B");
 htmlEl.classList.remove("my-nice-class--dimensions_A");
 */
 
-console.log("User agent: "+ userAgent + " android: "+android);
+//Elementos de cantidad en adiciones
+var ncharcant = document.getElementById("nchar-count");
+var nletrasmayuscant = document.getElementById("nletrasmayus-count");
+var nletrasminuscant = document.getElementById("nletrasminus-count");
+var nnumscant = document.getElementById("nnums-count");
+var nsimboloscant = document.getElementById("nsimbolos-count");
+var nnumsentrecharscant = document.getElementById("nnumsentrechars-count");
+var reqscant = document.getElementById("reqs-count");
+
+//Elementos de puntos bonus, para optimizar tiempo de acceso sobre memoria.
+var ncharbonus = document.getElementById("nchar-bonus");
+var nletrasmayusbonus = document.getElementById("nletrasmayus-bonus");
+var nletrasminusbonus = document.getElementById("nletrasminus-bonus");
+var nnumsbonus = document.getElementById("nnums-bonus");
+var nsimbolosbonus = document.getElementById("nsimbolos-bonus");
+var nnumsentrecharsbonus = document.getElementById("nnumsentrechars-bonus");
+var reqsbonusdom = document.getElementById("reqs-bonus");
+
+//Elemento de bruteforce
+var bruteforceElem = document.getElementById("dyToCrack");
+
+// --- CÓDIGO --- //
 
 function togglePwd(valorCheck){
     document.getElementById("passwordPwd").type = valorCheck ? 'text':'password';
 }
 
 function check(pwd){
+    //tiempo necesario de crackeo
     var daysToCrack = mellt.CheckPassword(document.getElementById("passwordPwd").value);
-    let doc = document.getElementById("dyToCrack");
 
+    /***** Parseo ******/
+    //vars
     let largoPwd = pwd.length;
     let cantLetrasMayus = 0;
     let cantLetrasMinus = 0;
@@ -31,9 +58,8 @@ function check(pwd){
     let numsEntreChars = 0;
     let reqs = 0;
 
-    /***** Parseo ******/
+    //aux
     let EntreChars = 0;
-    let type = "";
     for (const c of pwd) {
 
         if(((c>='a' && c<='z') || (c === 'á') || (c === 'é') || (c === 'í') || (c === 'ó') || (c === 'ú'))){
@@ -60,12 +86,23 @@ function check(pwd){
             else
                 numsEntreChars++;
         }
+
     }
 
     /****** Decisiones ******/
 
-    //Requerimientos: solo suma si hay al menos 3 de los tipos de caracteres y el largo es >= 8
-    let reqsBonus = 0;
+
+    //------- Cálculo de bonus -------//
+    //bonus total por categoría:
+    let bonuschars = largoPwd * 4;                                              //cant caracteres
+    let bonusmayus = (cantLetrasMayus) ? (largoPwd - cantLetrasMayus) * 2 : 0;  //cant mayusculas
+    let bonusminus = (cantLetrasMinus) ? (largoPwd - cantLetrasMinus) * 2 : 0;  //cant minúsculas
+    let bonusnum = cantNumeros * 4                                              //cant números
+    let bonussim = cantSimbolos * 6;                                            //cant símbolos
+    let bonusentrechars = numsEntreChars * 2;                                   //cant números/símbolos entre caracteres
+
+    //Requerimientos: 
+    let reqsBonus = 0;                                                          //bonus en función de requerimientos
     
     let tempReqs = 0;
     if(cantLetrasMinus){
@@ -82,56 +119,95 @@ function check(pwd){
     }
     reqs += tempReqs;
 
-    if( (largoPwd >=8) && (tempReqs>=3) ){
+    if( (largoPwd >=8) && (tempReqs>=3) ){                                      //solo sumo si paso los requerimientos mínimos
         reqs++;
         reqsBonus = reqs * 2;
     }
-    
 
+    //dias de bruteforce:
+    //automáticamente se pasan a años en caso de superar los 365 días
     if(daysToCrack == -1){
-        doc.innerHTML = "Dentro de las más comunes";
-        doc.classList = "";
-        doc.classList.add("center");
-        doc.classList.add("mellt-bad");
+        bruteforceElem.innerHTML = "Dentro de las más comunes";
+        bruteforceElem.classList = "";
+        bruteforceElem.classList.add("center");
+        bruteforceElem.classList.add("mellt-bad");
     }
     else if(daysToCrack == 0){
-            doc.innerHTML = "0 días";
-            doc.classList = "";
-            doc.classList.add("center");
-            doc.classList.add("mellt-bad");
+            bruteforceElem.innerHTML = "0 días";
+            bruteforceElem.classList = "";
+            bruteforceElem.classList.add("center");
+            bruteforceElem.classList.add("mellt-bad");
         }
         else if(daysToCrack < 365){
-            doc.innerHTML = daysToCrack + " días";
-            doc.classList = "";
-            doc.classList.add("center");
-            doc.classList.add("mellt-mild");
+            bruteforceElem.innerHTML = (daysToCrack === 1) ? daysToCrack + " día" : daysToCrack + " días";
+            bruteforceElem.classList = "";
+            bruteforceElem.classList.add("center");
+            bruteforceElem.classList.add("mellt-mild");
             }
             else{
                 daysToCrack = daysToCrack / 365;
-                doc.innerHTML = ""
-                doc.innerHTML = (daysToCrack < 2739726) ? Math.round(daysToCrack) + " años" : "Más de 27 millones de años";
-                doc.classList = "";
-                doc.classList.add("center");
-                (daysToCrack > 25000) ? doc.classList.add("mellt-exceptional"): doc.classList.add("mellt-good");
+                bruteforceElem.innerHTML = ""
+                bruteforceElem.innerHTML = (daysToCrack < 2739726) ? Math.round(daysToCrack) + " años" : "Más de 27 millones de años";
+                bruteforceElem.classList = "";
+                bruteforceElem.classList.add("center");
+                (daysToCrack > 25000) ? bruteforceElem.classList.add("mellt-exceptional"): bruteforceElem.classList.add("mellt-good");
             }
+
+    //------- Cálculo de deducciones -------//
 
 
     /****** Impresión a usuario ******/
 
-    //------- Adiciones
-    //---- Cantidad
-    document.getElementById("nchar-count").innerHTML = largoPwd;
-    document.getElementById("nletrasmayus-count").innerHTML = cantLetrasMayus;
-    document.getElementById("nletrasminus-count").innerHTML = cantLetrasMinus;
-    document.getElementById("nnums-count").innerHTML = cantNumeros;
-    document.getElementById("nsimbolos-count").innerHTML = cantSimbolos;
-    document.getElementById("nnumsentrechars-count").innerHTML = numsEntreChars;
-    document.getElementById("nnumsentrechars-count").innerHTML = numsEntreChars;
-    document.getElementById("reqs-count").innerHTML = reqs;
+    //------- Adiciones -------//
+    //---- Cantidad:
+    ncharcant.innerHTML = largoPwd;
+    nletrasmayuscant.innerHTML = cantLetrasMayus;
+    nletrasminuscant.innerHTML = cantLetrasMinus;
+    nnumscant.innerHTML = cantNumeros;
+    nsimboloscant.innerHTML = cantSimbolos;
+    nnumsentrecharscant.innerHTML = numsEntreChars;
+    reqscant.innerHTML = reqs;
 
-    //---- Bonus
-    document.getElementById("reqs-bonus").innerHTML = reqsBonus;
+    //---- Bonus:
+    ncharbonus.innerHTML = bonuschars;    //en 4 amarillo, en 8 verde, en 12 azul
+    if( (bonuschars >= 4) && (bonuschars < 8)  ) ncharbonus.setAttribute("value","good"); 
+    else if((bonuschars >= 8)) ncharbonus.setAttribute("value","excep"); 
+    else ncharbonus.setAttribute("value","bad"); 
 
+    nletrasmayusbonus.innerHTML = bonusmayus; // en 2 amarillo, en 4 verde, en 8 azul
+    if( (bonusmayus >= 4) && (bonusmayus < 8)  ) nletrasmayusbonus.setAttribute("value","good"); 
+    else if((bonusmayus >= 8) ) nletrasmayusbonus.setAttribute("value","excep"); 
+    else nletrasmayusbonus.setAttribute("value","bad"); 
+
+    nletrasminusbonus.innerHTML = bonusminus; // en 2 amarillo, en 4 verde, en 8 azul
+    if( (bonusminus >= 2) && (bonusminus < 4)  ) nletrasminusbonus.setAttribute("value","good"); 
+    else if((bonusminus >= 4)) nletrasminusbonus.setAttribute("value","excep"); 
+    else nletrasminusbonus.setAttribute("value","bad"); 
+
+    nnumsbonus.innerHTML = bonusnum;     //en 4 amarillo, 8 verde, 12 azul
+    if( (bonusnum >= 4) && (bonusnum < 8)  ) nnumsbonus.setAttribute("value","good"); 
+    else if((bonusnum >= 8)) nnumsbonus.setAttribute("value","excep"); 
+    else nnumsbonus.setAttribute("value","bad"); 
+
+    nsimbolosbonus.innerHTML = bonussim; //en 6 amarillo, 12 verde, 18 azul
+    if( (bonussim >= 6) && (bonussim < 12)  ) nsimbolosbonus.setAttribute("value","good"); 
+    else if((bonussim >= 12) ) nsimbolosbonus.setAttribute("value","excep"); 
+    else nsimbolosbonus.setAttribute("value","bad"); 
+
+    nnumsentrecharsbonus.innerHTML = numsEntreChars * 2; // en 2 amarillo, 8 verde, 12 azul
+    if( (bonusentrechars >= 2) && (bonusentrechars < 8)  ) nnumsentrecharsbonus.setAttribute("value","good"); 
+    else if((bonusentrechars >= 8) ) nnumsentrecharsbonus.setAttribute("value","excep"); 
+    else nnumsentrecharsbonus.setAttribute("value","bad"); 
+
+    reqsbonusdom.innerHTML = reqsBonus; //8 verde, 10 azul
+    if( (reqsBonus >= 4) && (reqsBonus < 8)  ) reqsbonusdom.setAttribute("value","good"); 
+    else if((reqsBonus >= 8) ) reqsbonusdom.setAttribute("value","excep"); 
+    else reqsbonusdom.setAttribute("value","bad"); 
+
+    //------- Deducciones -------//
+    //---- Cantidad:
+
+    //---- Deducción:
 
     //para los colores usar css en base al value ? lo que iba con []
 }
